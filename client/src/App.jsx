4973265d28eb502501a330ddbd790d65caba6cce
stephramos <stephaniemnz@gmail.com>
2024-04-15 20:01:09 -0500

    added login.jsx, home.jsx, and purchase.jsx and added stripe for payment

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,60 +1,26 @@
 import { useState } from 'react';
 import './App.css';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Purchase from './pages/Purchase';
 
 function App() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleEmailChange = (event) => {
-    setEmail(event.target.value);
-  };
-
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-  };
-
-  const handleLoginSubmit = (event) => {
-    event.preventDefault();
-    console.log('Login Submitted');
-    console.log('Email:', email);
-    console.log('Password:', password);
-    // Here you can add more logic for actual authentication
-  };
-
-  const handleSignUpClick = () => {
-    console.log('Sign Up button clicked');
-    // Add your logic for navigating to the sign up page or displaying a sign up form
-  };
-
   return (
-    <>
-      <div className="card">
-        <form onSubmit={handleLoginSubmit} style={{ marginTop: '400px' }}>
-          <div>
-            <label htmlFor="email">Email:</label>
-            <input
-              type="email"
-              id="email"
-              value={email}
-              onChange={handleEmailChange}
-              required
-            />
-          </div>
-          <div>
-            <label htmlFor="password">Password:</label>
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={handlePasswordChange}
-              required
-            />
-          </div>
-          <button type="submit">Login</button>
-        </form>
-        <button onClick={handleSignUpClick} style={{ marginTop: '10px' }}>Sign Up</button>
+    <Router>
+      <div>
+        <switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/purchase">
+            <Purchase />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>  
+        </switch>   
       </div>
-    </>
+   </Router>
   );
 }
 
