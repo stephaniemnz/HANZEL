ac9650a21b04f247b5da8898e087450b155688d1
Ryan Hanzel <100760416+H3Xivall@users.noreply.github.com>
2024-04-16 21:54:13 -0500

    made some adjustments with server.js and index.html and main.jsx to get the npm build command to work - temporary adjustments and will need to be remedied at a later time

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import 'semantic-ui-css/semantic.min.css'
+//import 'semantic-ui-css/semantic.min.css'
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Purchase from "./pages/Purchase";
-import Cart from "./pages/Cart";
+// import Home from "./pages/Home";
+// import Login from "./pages/Login";
+// import Purchase from "./pages/Purchase";
+// import Cart from "./pages/Cart";
 
 const router = createBrowserRouter([
   {
