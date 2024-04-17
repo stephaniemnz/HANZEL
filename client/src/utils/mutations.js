617c338cba4355a6dd47746f6b86b615e9aa27b5
Ryan Hanzel <100760416+H3Xivall@users.noreply.github.com>
2024-04-16 19:35:55 -0500

    main adjustments2

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,1 +1,13 @@
-export const LOGIN = gql` 
+import { gql } from '@apollo/client';
+
+export const LOGIN = gql`
+  mutation Login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      token
+      user {
+        id
+        name
+      }
+    }
+  }
+`;
